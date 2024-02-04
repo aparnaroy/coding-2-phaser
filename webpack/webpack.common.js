c45ce0097a300f44cda6e6fb39d99d0f1619b0e3
Austin Cory Bart <acbart@vt.edu>
2024-02-04 13:04:14 -0500

    Reorganize filesystem

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,40 +1,49 @@
-const path = require('path')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: ['./src/scripts/game.ts', './webpack/credits.js'],
-  output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].chunk.js'
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js']
-  },
-  module: {
-    rules: [{ test: /\.tsx?$|\.jsx?$/, include: path.join(__dirname, '../src'), loader: 'ts-loader' }]
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-          filename: '[name].bundle.js'
-        }
-      }
-    }
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ gameName: 'My Phaser Game', template: 'src/index.html' }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'src/assets', to: 'assets' },
-        { from: 'pwa', to: '' },
-        { from: 'src/favicon.ico', to: '' }
-      ]
-    })
-  ]
-}
+    entry: ["./src/game.ts", "./webpack/credits.js"],
+    output: {
+        path: path.resolve(__dirname, "../dist"),
+        filename: "[name].bundle.js",
+        chunkFilename: "[name].chunk.js",
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$|\.jsx?$/,
+                include: path.join(__dirname, "../src"),
+                loader: "ts-loader",
+            },
+        ],
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: "vendors",
+                    chunks: "all",
+                    filename: "[name].bundle.js",
+                },
+            },
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            gameName: "My Phaser Game",
+            template: "assets/index.html",
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: "assets", to: "assets" },
+                { from: "pwa", to: "" },
+                { from: "assets/favicon.ico", to: "" },
+            ],
+        }),
+    ],
+};
