ff006a994b814fa03aab31529429a4463493b505
aparnaroy <aparnar@udel.edu>
2024-02-25 11:06:21 -0500

    Finished Part 2

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -2,6 +2,8 @@ import Phaser from "phaser";
 
 export default class MainScene extends Phaser.Scene {
     private platforms?: Phaser.Physics.Arcade.StaticGroup;
+    private player?: Phaser.Physics.Arcade.Sprite;
+    private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
 
     constructor() {
         super({ key: "MainScene" });
@@ -21,7 +23,60 @@ export default class MainScene extends Phaser.Scene {
         this.platforms.create(600, 400, "ground");
         this.platforms.create(50, 250, "ground");
         this.platforms.create(750, 220, "ground");
+
+        this.player = this.physics.add.sprite(100, 450, "dude");
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        // Specify spritesheet frames for the dude walking left, right, and turning
+        this.anims.create({
+            key: "left",
+            frames: this.anims.generateFrameNumbers("dude", {
+                start: 0,
+                end: 3,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: "turn",
+            frames: [{ key: "dude", frame: 4 }],
+            frameRate: 20,
+        });
+
+        this.anims.create({
+            key: "right",
+            frames: this.anims.generateFrameNumbers("dude", {
+                start: 5,
+                end: 8,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+        // Specify that the dude should collide with / be on the ground/platform
+        this.physics.add.collider(this.player, this.platforms);
+
+        this.cursors = this.input.keyboard?.createCursorKeys();
     }
 
-    update() {}
+    update() {
+        // If left key is pressed, start dude's left animation, ignore if playing is already true so we don't restart the animation
+        if (this.cursors?.left.isDown) {
+            this.player?.setVelocityX(-160);
+            this.player?.anims.play("left", true);
+        } else if (this.cursors?.right.isDown) {
+            this.player?.setVelocityX(160);
+            this.player?.anims.play("right", true);
+        } else {
+            this.player?.setVelocityX(0);
+            this.player?.anims.play("turn");
+        }
+
+        // If up key pressed and dude is on ground, make dude jump
+        if (this.cursors?.up.isDown && this.player?.body?.touching.down) {
+            this.player.setVelocityY(-330);
+        }
+    }
 }
